e08f81d501d83c104089e5aa158a923e7509cbca
Ayush  Mahakulkar <115718908+Ayush935@users.noreply.github.com>
2023-10-29 09:26:19 +0000

    container config done

--- 1_basics/datatypes.js
+++ 1_basics/datatypes.js
@@ -20,3 +20,43 @@ console.log(typeof null)
 console.log(typeof age)
 console.log(typeof name)
 console.log(typeof isLogged)
+
+
+// primmitive and non-primitive datatypes   // in js they there are two types of datatype
+
+//primmitive
+// call by value , copy is made and given to u
+// 7 types:  String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// is js dynamic or static ?
+// js 
+
+
+// Symbol 
+const id = Symbol('21')
+const anotherId = Symbol('21')
+console.log(id === anotherId)
+
+// Reference (Non-primitive)
+// memory allocate the reference 
+// array, objects , functions
+
+const bigNumber = 1564646496464886464n
+
+const heroes = ["spiderman", "Batman", "Superman"]
+let myObj = {
+    name: "Ayush",
+    age: 18,
+}
+
+const myFunction = function(){
+     console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
+
+myFunction();
+
+console.log(myObj)
+console.log(heroes)
