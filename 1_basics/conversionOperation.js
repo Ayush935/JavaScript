f246afb655a1e4b85cd9e69e4d666e451da9efaf
Ayush  Mahakulkar <115718908+Ayush935@users.noreply.github.com>
2023-10-29 08:47:54 +0000

    container config done

---
+++ 1_basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+let s = "abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(s)
+
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber1)   // number
+
+console.log(valueInNumber1)  // NaN
+
+let a = ""// 5=>true 0=>false string " "=>true  string "" => false  string "dd" =>true
+let p = Boolean(a)
+console.log(p)
+
+let z=5
+let x= String(z)
+console.log(x)
+console.log(typeof x)
