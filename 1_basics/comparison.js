a5ba7e3dbafd803dea5166d1bcca5022b73db216
Ayush  Mahakulkar <115718908+Ayush935@users.noreply.github.com>
2023-10-29 09:05:58 +0000

    container config done

---
+++ 1_basics/comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1)
+console.log("2">1) // This is not a good practise , we should compare wiith same datatype
+console.log("02">1)
+
+console.log(null > 0)
+console.log(null == 0)  //assigns value in js
+console.log(null >= 0)  // so it's coming true
+
+
+// ===
+
+console.log("2"===2)
