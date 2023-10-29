af16b009409d80af8879490e9ef64706a04c7ab1
Ayush  Mahakulkar <115718908+Ayush935@users.noreply.github.com>
2023-10-29 08:15:06 +0000

    container config done

---
+++ 1_basics/variables.js
@@ -0,0 +1,20 @@
+const SchoolId = 1231
+let ScPassword = "123546"
+var emaiId = "shdb@gmail.com"
+accCity = "Pune"
+let accData
+
+// SchoolId = 2
+// const cant be change
+
+ScPassword = "466"
+emaiId = "sj@gmail.com"
+accCity = "Mumbai"
+
+console.log(SchoolId)
+
+/*
+  prefer not to use var because of block scope and function scope
+*/
+
+console.table([SchoolId , ScPassword , emaiId , accCity , accData])
