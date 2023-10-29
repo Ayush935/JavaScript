b71399d669ba996210386aa424b94ad8babeee9a
Ayush  Mahakulkar <115718908+Ayush935@users.noreply.github.com>
2023-10-29 08:31:37 +0000

    container config done

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,22 @@
+" use strict"; // treat all JS code as newer version
+
+// alert(3+3)   // we are using nodejs , not browser
+
+let age = 20
+let name = "Ayush"
+let isLogged = false
+
+// number (range 2 to power 53)
+// bigint for very big number
+// string => ""
+// symbol => unique => we use in figma
+// null => object
+// undefined => undefined
+// boolean => true/false
+
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isLogged)
